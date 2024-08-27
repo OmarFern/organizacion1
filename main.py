49b5d707a375f33f7ccc22a80009524c8848b481
omar <construredomar@gmail.com>
2024-08-27 15:15:39 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def sumar(a, b):
+    return a + b
+
+def main():
+    num1 = 5
+    num2 = 10
+    resultado = sumar(num1, num2)
+    print("El resultado de la suma es:", resultado)
+
+if __name__ == "__main__":
+    main()
