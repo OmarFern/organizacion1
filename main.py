c657b0618954f655cf3faadab1c290dde52f8091
omar <construredomar@gmail.com>
2024-08-27 16:56:04 -0300

    add  funcion resta

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 def sumar(a, b):
     return a + b
 
+def resta(a, b):
+    return a + b
+
 def main():
     num1 = 5
     num2 = 10
-    resultado = sumar(num1, num2)
-    print("El resultado de la suma es:", resultado)
+    resultado_suma = sumar(num1, num2)
+    resultado_resta = resta(num1, num2)
+    print("El resultado de la suma es:", resultado_suma)
+    print("El resultado de la suma es:", resultado_resta)
+
+
+
 
 if __name__ == "__main__":
     main()
